a6e9ed32e336d4405ed531701ff2b5d3a4982908
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2018-07-25 09:24:15 +0200

    Add flake8 to dev-requirements

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -85,6 +85,7 @@ cd -
 echo "Installing ckanext-stadtzh-harvest and its requirements..."
 python setup.py develop
 pip install -r requirements.txt
+pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
