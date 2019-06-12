dce04432f147b06b66055d42719232c4e63116d7
Stefanie Taepke <stefanie.taepke@liip.ch>
2019-06-12 16:52:43 +0200

    return true if deleting was successful so we can track errors

--- ckanext/stadtzhharvest/tests/test_harvester.py
+++ ckanext/stadtzhharvest/tests/test_harvester.py
@@ -574,7 +574,6 @@ class TestStadtzhHarvestFunctional(FunctionalHarvestTest):
         app = h._get_test_app()
         response = app.get(url, status=200)
 
-
     def test_delete_dataset(self):
         data_path = os.path.join(
             __location__,
@@ -653,7 +652,6 @@ class TestStadtzhHarvestFunctional(FunctionalHarvestTest):
         eq_(last_job_status['stats']['deleted'], 2)
         eq_(last_job_status['stats']['not modified'], 0)
         eq_(last_job_status['stats']['errored'], 0)
-
 
     def test_harvest_update_dwh(self):
         data_path = os.path.join(
