f93b8f0db4918666c350b1bab515d0a3d18f472b
Rae Knowler <rae.knowler@liip.ch>
2023-12-04 13:46:21 +0100

    tests: Move fixtures __init__.py to the right place

---
+++ ckanext/stadtzhharvest/tests/fixtures/__init__.py
@@ -0,0 +1,16 @@
+# Copied from ckanext-harvester.
+
+import pytest
+
+import ckanext.harvest.model as harvest_model
+from ckanext.harvest import queue
+
+
+@pytest.fixture
+def harvest_setup():
+    harvest_model.setup()
+
+
+@pytest.fixture
+def clean_queues():
+    queue.purge_queues()
