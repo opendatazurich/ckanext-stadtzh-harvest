862708db7227b1493a3f9650685569fce812d1c8
Rae Knowler <rae.knowler@liip.ch>
2024-02-07 15:09:09 +0100

    tests: Migrate db for harvest plugin if applicable
    If we get an alembic error, we are using a version of ckanext-harvest before they switched to alembic migrations.

--- ckanext/stadtzhharvest/tests/fixtures/__init__.py
+++ ckanext/stadtzhharvest/tests/fixtures/__init__.py
@@ -1,6 +1,7 @@
 # Copied from ckanext-harvester.
 
 import pytest
+from alembic.util import CommandError
 
 from ckanext.harvest import queue
 
@@ -8,6 +9,12 @@ from ckanext.harvest import queue
 @pytest.fixture
 def clean_db(reset_db, migrate_db_for):
     reset_db()
+    try:
+        migrate_db_for("harvest")
+    except CommandError:
+        # ckanext-harvest has switched to using Alembic migrations, but this change
+        # is not yet released: https://github.com/ckan/ckanext-harvest/pull/540
+        pass
 
 
 @pytest.fixture
