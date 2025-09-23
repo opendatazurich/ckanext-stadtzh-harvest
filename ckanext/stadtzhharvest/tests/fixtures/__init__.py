d8859e169d8c36956061d5ae61bd555d9885a933
Rae Knowler <rae.knowler@liip.ch>
2025-09-23 14:56:48 +0200

    tests: Update pytest fixtures for CKAN 2.11

--- ckanext/stadtzhharvest/tests/fixtures/__init__.py
+++ ckanext/stadtzhharvest/tests/fixtures/__init__.py
@@ -1,38 +1,21 @@
-# Copied from ckanext-harvester.
-
 import pytest
-from alembic.util import CommandError
 from ckan.tests.helpers import config
 
-import ckanext.harvest.model as harvest_model
 from ckanext.harvest import queue
-from ckanext.stadtzhtheme.plugin import StadtzhThemePlugin
+from ckanext.stadtzhtheme.plugin import (
+    create_dataType,
+    create_updateInterval,
+)
 
 
 @pytest.fixture
 def clean_db(reset_db, migrate_db_for):
     reset_db()
-
-    try:
-        migrate_db_for("harvest")
-    except CommandError:
-        # ckanext-harvest has switched to using Alembic migrations, but this change
-        # is not yet released: https://github.com/ckan/ckanext-harvest/pull/540
-        pass
+    migrate_db_for("harvest")
 
     # Cleaning the db gets rid of our custom tag vocabularies, so create them again
-    plugin = StadtzhThemePlugin()
-    plugin.configure(config=config)
-
-
-@pytest.fixture
-def harvest_setup():
-    try:
-        harvest_model.setup()
-    except AttributeError:
-        # harvest_model.setup() has been removed in the new version of ckanext-harvest
-        # https://github.com/ckan/ckanext-harvest/pull/540
-        pass
+    create_dataType()
+    create_updateInterval()
 
 
 @pytest.fixture
