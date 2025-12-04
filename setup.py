3867a01ee9cb23c4abfe1e8bc82666b9507d9e82
Stefanie Taepke <stefina@users.noreply.github.com>
2025-12-04 14:24:21 +0100

    feat: use a compatible license to the deprecated license that was in use before

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Liip AG",
     author_email="ogd@liip.ch",
     url="http://www.liip.ch",
-    license="GPL",
+    license="AGPL-3.0-or-later",
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     namespace_packages=["ckanext"],
     include_package_data=True,
